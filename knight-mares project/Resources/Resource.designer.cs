783a053566f5392bda8a16b4a87c2753e1106574
schoolrupin00 <schoolrupin00@gmail.com>
2022-01-19 15:30:53 +0200

    start adding menu items

--- knight-mares project/Resources/Resource.designer.cs
+++ knight-mares project/Resources/Resource.designer.cs
@@ -4128,73 +4128,79 @@ namespace knight_mares_project
 			public const int touch_outside = 2131230951;
 			
 			// aapt resource value: 0x7F0800E8
-			public const int transition_current_scene = 2131230952;
+			public const int tour = 2131230952;
 			
 			// aapt resource value: 0x7F0800E9
-			public const int transition_layout_save = 2131230953;
+			public const int transition_current_scene = 2131230953;
 			
 			// aapt resource value: 0x7F0800EA
-			public const int transition_position = 2131230954;
+			public const int transition_layout_save = 2131230954;
 			
 			// aapt resource value: 0x7F0800EB
-			public const int transition_scene_layoutid_cache = 2131230955;
+			public const int transition_position = 2131230955;
 			
 			// aapt resource value: 0x7F0800EC
-			public const int transition_transform = 2131230956;
+			public const int transition_scene_layoutid_cache = 2131230956;
 			
 			// aapt resource value: 0x7F0800ED
-			public const int tv1 = 2131230957;
+			public const int transition_transform = 2131230957;
 			
 			// aapt resource value: 0x7F0800EE
-			public const int tv2 = 2131230958;
+			public const int tutorial = 2131230958;
 			
 			// aapt resource value: 0x7F0800EF
-			public const int tv3 = 2131230959;
+			public const int tv1 = 2131230959;
 			
 			// aapt resource value: 0x7F0800F0
-			public const int tvDisplayDifficulty = 2131230960;
+			public const int tv2 = 2131230960;
 			
 			// aapt resource value: 0x7F0800F1
-			public const int tvTime = 2131230961;
+			public const int tv3 = 2131230961;
 			
 			// aapt resource value: 0x7F0800F2
-			public const int tvTitle = 2131230962;
+			public const int tvDisplayDifficulty = 2131230962;
 			
 			// aapt resource value: 0x7F0800F3
-			public const int tvWinMessage = 2131230963;
+			public const int tvTime = 2131230963;
 			
 			// aapt resource value: 0x7F0800F4
-			public const int @unchecked = 2131230964;
+			public const int tvTitle = 2131230964;
 			
 			// aapt resource value: 0x7F0800F5
-			public const int uniform = 2131230965;
+			public const int tvWinMessage = 2131230965;
 			
 			// aapt resource value: 0x7F0800F6
-			public const int unlabeled = 2131230966;
+			public const int @unchecked = 2131230966;
 			
 			// aapt resource value: 0x7F0800F7
-			public const int up = 2131230967;
+			public const int uniform = 2131230967;
 			
 			// aapt resource value: 0x7F0800F8
-			public const int useLogo = 2131230968;
+			public const int unlabeled = 2131230968;
 			
 			// aapt resource value: 0x7F0800F9
-			public const int view_offset_helper = 2131230969;
+			public const int up = 2131230969;
 			
 			// aapt resource value: 0x7F0800FA
-			public const int view_tree_saved_state_registry_owner = 2131230970;
+			public const int useLogo = 2131230970;
 			
 			// aapt resource value: 0x7F0800FB
-			public const int visible = 2131230971;
+			public const int view_offset_helper = 2131230971;
 			
 			// aapt resource value: 0x7F0800FC
-			public const int visible_removing_fragment_view_tag = 2131230972;
+			public const int view_tree_saved_state_registry_owner = 2131230972;
 			
 			// aapt resource value: 0x7F0800FD
-			public const int withText = 2131230973;
+			public const int visible = 2131230973;
 			
 			// aapt resource value: 0x7F0800FE
-			public const int wrap_content = 2131230974;
+			public const int visible_removing_fragment_view_tag = 2131230974;
+			
+			// aapt resource value: 0x7F0800FF
+			public const int withText = 2131230975;
+			
+			// aapt resource value: 0x7F080100
+			public const int wrap_content = 2131230976;
 			
 			static Id()
 			{
